af77136a2628c68878f0515d9c2ca8cbd9b3a5c8
Jared Palmer <jaredpalmer>
2016-07-14 17:50:47 -0400

    Use GrapheneDB

--- server/models/db.js
+++ server/models/db.js
@@ -1,3 +1,6 @@
 const neo4j = require('neo4j-driver').v1
-const driver = neo4j.driver(process.env.GRAPHENEDB_URL || 'bolt://localhost', neo4j.auth.basic('neo4j', 'neo5j'))
+const URL = require('url')
+const url = URL.parse(process.env.GRAPHENEDB_URL)
+console.log(url)
+const driver = neo4j.driver(`bolt://${url.host}`, neo4j.auth.basic(url.auth.split(':')[0], url.auth.split(':')[1]))
 export default driver
