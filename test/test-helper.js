53cb12203e0ab733d73c6edd9a7d7d07d2bf2243
Jared Palmer <jaredpalmer>
2016-08-26 12:33:35 -0400

    Fix tests

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,15 +1,13 @@
-import db from '../server/models/db'
+import neo4j from 'neo4j'
+
+const db = new neo4j.GraphDatabase({
+  url: process.env.GRAPHENEDB_URL || 'http://neo4j:neo5j@localhost:7474'
+})
 
 export const resetDB = (done) => {
-  const session = db.session()
-  session
-    .run('MATCH (n) DETACH DELETE n')
-    .then(result => {
-      done()
-      session.close()
-    })
-    .catch(e => {
-      console.error(e)
-      done()
-    })
+  const query = 'MATCH (n) DETACH DELETE n'
+  db.cypher({ query }, (err, result) => {
+    if (err) done(err)
+    done()
+  })
 }
