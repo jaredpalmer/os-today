5b762ca1807a473896a54b5ffeb949a6cdbb5dc4
Jared Palmer <jaredpalmer>
2016-08-31 10:27:21 -0400

    Add basic infinite scrolling. Closes #24 .

--- client/containers/Feed.js
+++ client/containers/Feed.js
@@ -1,56 +1,75 @@
 /* global fetch */
 import React from 'react'
-import {withRouter} from 'react-router'
 import RepoList from '../components/RepoList'
-import Button from '../components/Button'
 
 class Feed extends React.Component {
   constructor () {
     super()
     this.state = {
-      isLoading: false,
       repos: [],
-      page: 0
+      page: 0,
+      paging: false,
+      done: false
     }
-    this.loadRepos = this.loadRepos.bind(this)
+    this.checkWindowScroll = this.checkWindowScroll.bind(this)
+    this.getPage = this.getPage.bind(this)
   }
 
-  componentDidMount () {
-    this.setState({ isLoading: true, repos: [] })
+  checkWindowScroll () {
+    // Get scroll pos & window data
+    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    const s = document.body.scrollTop
+    const scrolled = (h + s) + 300 > document.body.offsetHeight
+
+    // If scrolled enough, not currently paging and not complete...
+    if (scrolled && !this.state.paging && !this.state.done) {
+      // Set application state (Paging, Increment page)
+      this.setState({paging: true, page: this.state.page + 1})
+
+      // Get the next page of repos from the server
+      this.getPage(this.state.page)
+    }
+  }
+
+  getPage (page) {
     fetch(`/api/v0/users/feed/${this.state.page}`, {
       credentials: 'include'
     })
     .then(res => res.json())
-    .then(res => this.setState({repos: res, isLoading: false}))
+    .then(res => {
+      if (res.length > 0) {
+        const updatedRepos = this.state.repos
+        res.forEach(repo => updatedRepos.push(repo))
+        this.setState({ repos: updatedRepos, paging: false })
+      } else {
+        this.setState({ paging: false, done: true })
+      }
+    }).catch(e => {
+      console.log(e)
+      this.setState({ paging: false, done: true })
+    })
   }
 
-  loadRepos () {
-    this.setState({ isLoading: true, repos: [] })
-    fetch(`/api/v0/users/feed/${this.state.page + 1}`, {
-      credentials: 'include'
-    })
-    .then(res => res.json())
-    .then(res => this.setState({ repos: res, isLoading: false, page: this.state.page + 1 }))
+  componentDidMount () {
+    window.addEventListener('scroll', this.checkWindowScroll)
+    this.getPage(this.state.page)
   }
 
   render () {
     return (
       <div>
-        {this.state.isLoading &&
-          <div style={{margin: '1rem', fontWeight: '900', fontSize: '1.5rem'}}>Loading....</div>
-        }
-        {!this.state.isLoading && !this.state.repos.length > 0 &&
+        {this.state.repos.length === 0 &&
           <div style={{margin: '1rem', fontWeight: '900', fontSize: '1.5rem'}}>Go star some stuff!</div>
         }
-        {!this.state.isLoading && this.state.repos.length > 0 &&
+        {this.state.repos.length > 0 &&
           <RepoList repos={this.state.repos} />
         }
-        <div style={{margin: '0 auto 4rem', padding: '0 1rem'}}>
-          <Button onClick={this.loadRepos}>Next</Button>
-        </div>
+        {this.state.paging &&
+          <div style={{margin: '1rem', fontWeight: '900', fontSize: '1.5rem'}}>Loading....</div>
+        }
       </div>
     )
   }
 }
 
-export default withRouter(Feed)
+export default Feed
